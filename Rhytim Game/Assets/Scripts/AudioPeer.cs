065b2d3cde86d8d8b633e1bdf8882644c21195fe
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-01-09 17:30:22 +0100

    Started with Rhytim game

---
+++ Rhytim Game/Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPeer : MonoBehaviour
+{
+    static int DEFAULT_SAMPLE_SIZE = 512;
+
+    AudioSource audioSource;
+    public static float[] samples = new float[DEFAULT_SAMPLE_SIZE];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ Rhytim Game/Assets/Scripts/NoteObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    bool canBePressed = false;
+
+    public float beatTempo;
+    public KeyCode keyPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0f, 0f, beatTempo * Time.deltaTime);
+
+        if(Input.GetKeyDown(keyPressed))
+        {
+            if (canBePressed)
+                Destroy(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Button")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Button")
+        {
+            canBePressed = false;
+        }
+    }
+}
